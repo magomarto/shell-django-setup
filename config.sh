6c725f0fcbf208f29b2e96694c583c0477addad8
Lucas <lucashravazi55@gmail.com>
2024-08-15 16:24:01 -0300

    variables implants

--- config.sh
+++ config.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+envName="my_env"
+projectName="my_project"
+appName="my_app"
+# adminName="admin"
+# adminEmail="admin@example.com"
+# adminPsswd="admin"
+
 error_exit() {
     echo "$1" 1>&2
     exit 1
@@ -12,10 +19,10 @@ echo "#### Installing python, pip, and virtualenv... ####"
 sudo apt install python3 python3-pip python3-venv || error_exit
 
 echo "#### Creating the virtual environment... ####"
-python3 -m venv my_env || error_exit
+python3 -m venv $envName || error_exit
 
 echo "#### Activating the virtual environment... ####"
-source my_env/bin/activate || error_exit
+source $envName/bin/activate || error_exit
 
 echo "#### Installing django... ####"
 pip install django 
@@ -24,12 +31,12 @@ echo "#### Creating requirements.txt... ####"
 pip freeze > requirements.txt
 
 echo "#### Creating django project... ####"
-django-admin startproject myproject
-cd myproject
+django-admin startproject $projectName
+cd $projectName
 
 echo "#### Creating django app ####"
-python3 manage.py startapp my_app
-cd my_app
+python3 manage.py startapp $appName
+cd $appName
 mkdir templates
 mkdir static
 cd static
@@ -46,6 +53,5 @@ python3 manage.py migrate
 
 echo "#### Creating superuser... ####"
 python3 manage.py createsuperuser
-
 
 python3 manage.py runserver
